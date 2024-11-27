2def7eb90d455e961369d44182812d9180d4cf0f
hayim.deutsch <chayimdeutsch@gmail.com>
2024-11-28 01:20:52 +0200

    Move token to header

--- back/middleware/authenticateAccessToken.js
+++ back/middleware/authenticateAccessToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const authenticateAccessToken = (req, res, next) => {
-    const token = req.cookies.accessToken;
+    const token = req?.headers?.authorization?.split(' ')[1];
     if (!token) {
         return res.status(401).json({msg: "No token provided"});
     }
