55d514a2071b55c11d93faa6f224e9c0b6ac571d
hayim.deutsch <chayimdeutsch@gmail.com>
2025-01-21 11:22:33 +0200

    Delete commented out code

--- front/src/hooks/useProtectedFetch.js
+++ front/src/hooks/useProtectedFetch.js
@@ -1,42 +1,3 @@
-// import { useState, useEffect } from "react";
-// import useAxiosProtected from "./useAxiosProtected";
-
-// export default function useProtectedFetch(endpoint, refresh) {
-//   let privateAxios = useAxiosProtected();
-
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     let abortController = new AbortController();
-//     const fetchData = async () => {
-//       try {
-//         const response = await privateAxios(endpoint, {
-//           signal: abortController.signal,
-//         });
-//         setLoading(false);
-//         setData(response.data);
-//         setError(null);
-//       } catch (error) {
-//         if (error?.response?.status !== 401) {
-//           setError(error);
-//           setLoading(false);
-          
-//         }
-//       }
-//     };
-
-//     fetchData();
-    
-//     return () => {
-//       abortController.abort();
-//     };
-//   }, [endpoint, refresh]);
-
-//   return { data, loading, error };
-// }
-
 import { useState, useEffect } from "react";
 import useAxiosProtected from "./useAxiosProtected";
 
@@ -47,31 +8,31 @@ export default function useProtectedFetch(endpoint, refresh) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-useEffect(() => {
-  let abortController = new AbortController();
-  const fetchData = async () => {
-    try {
-      const response = await privateAxios(endpoint, {
-        signal: abortController.signal,
-      });
-      setData(response.data);
-      setError(null);
-    } catch (error) {
-      if (!abortController.signal.aborted) {
-        setError(error);
+  useEffect(() => {
+    let abortController = new AbortController();
+    const fetchData = async () => {
+      try {
+        const response = await privateAxios(endpoint, {
+          signal: abortController.signal,
+        });
+        setData(response.data);
+        setError(null);
+      } catch (error) {
+        if (!abortController.signal.aborted) {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
       }
-    } finally {
-      setLoading(false);
-    }
-  };
+    };
 
-  const timer = setTimeout(fetchData, 300); // debounce for 300ms
+    const timer = setTimeout(fetchData, 300);
 
-  return () => {
-    clearTimeout(timer);
-    abortController.abort();
-  };
-}, [endpoint, refresh]);
+    return () => {
+      clearTimeout(timer);
+      abortController.abort();
+    };
+  }, [endpoint, refresh]);
 
   return { data, loading, error };
 }
